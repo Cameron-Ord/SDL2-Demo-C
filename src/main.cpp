16e6a87e112dde33695c28c6ec90a275d8464d57
Cameron-Ord <cameron.ian.ronald.ord@Outlook.com>
2023-08-08 17:32:19 -0600

    2000ms window open

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,12 @@
-#include <iostream>
+#include <SDL2/SDL.h>
 
-int main(int, char**){
-    std::cout << "Hello, from sdl_demo!\n";
+int main(int argc, char ** arg)
+{
+    SDL_Init(SDL_INIT_EVERYTHING);
+    SDL_Window * win = SDL_CreateWindow("title", 30, 30, 600, 500, SDL_WINDOW_SHOWN);
+    SDL_Surface * screen = SDL_GetWindowSurface(win);
+    SDL_Delay(2000);
+    SDL_DestroyWindow(win);
+    SDL_Quit();
+
 }

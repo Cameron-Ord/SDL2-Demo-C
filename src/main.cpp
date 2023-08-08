bfbfa3bb9b352bca2fdd8cff5faaf350eba74429
Cameron-Ord <cameron.ian.ronald.ord@Outlook.com>
2023-08-08 17:37:50 -0600

    event handling

--- src/main.cpp
+++ src/main.cpp
@@ -5,7 +5,22 @@ int main(int argc, char ** arg)
     SDL_Init(SDL_INIT_EVERYTHING);
     SDL_Window * win = SDL_CreateWindow("title", 30, 30, 600, 500, SDL_WINDOW_SHOWN);
     SDL_Surface * screen = SDL_GetWindowSurface(win);
-    SDL_Delay(2000);
+
+    bool run = true;
+    while(run)
+    {
+        SDL_Event ev;
+        while(SDL_PollEvent(&ev))
+        {
+            switch(ev.type)
+            {
+                case SDL_QUIT:
+                run = false;
+                break;
+            }
+        }
+    }
+
     SDL_DestroyWindow(win);
     SDL_Quit();
 

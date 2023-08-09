6ea1da2c6d05699cb924d27dd79164243da8e5dd
Cameron-Ord <cameron.ian.ronald.ord@Outlook.com>
2023-08-08 19:47:45 -0600

    framework

--- src/main.cpp
+++ src/main.cpp
@@ -1,10 +1,12 @@
 #include <SDL2/SDL.h>
-
+#include "item.h"
 int main(int argc, char ** arg)
 {
     SDL_Init(SDL_INIT_EVERYTHING);
     SDL_Window * win = SDL_CreateWindow("title", 30, 30, 600, 500, SDL_WINDOW_SHOWN);
     SDL_Surface * screen = SDL_GetWindowSurface(win);
+
+    item bob;
 
     bool run = true;
     while(run)
@@ -19,6 +21,11 @@ int main(int argc, char ** arg)
                 break;
             }
         }
+
+        SDL_FillRect(screen, NULL, 0x00aaff);
+        bob.draw(screen);
+        SDL_UpdateWindowSurface(win);
+
     }
 
     SDL_DestroyWindow(win);
